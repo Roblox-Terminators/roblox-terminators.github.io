dfad5e16079db886d9a453f150e5e735d7f0c16a
cryoxe <71180076+cryoxe@users.noreply.github.com>
2024-04-01 19:32:35 +0200

    Changed "unit" test name

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('RobloxInDoc', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Roblox/i);
   expect(linkElement).toBeInTheDocument();
 });
