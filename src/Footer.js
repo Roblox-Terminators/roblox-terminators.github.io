8ebf4f916fe57332bff7221737ea295a9c571b04
cryoxe <71180076+cryoxe@users.noreply.github.com>
2024-04-03 20:26:56 +0200

    Created a file for the footer

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import 'bulma/css/bulma.min.css';
+import './Arrow.css';
+
+function Footer() {
+    return (
+        <footer class="footer">
+        <div class="content has-text-centered">
+        <p>
+          By the Roblox Terms Team.
+        </p>
+        </div>
+      </footer>
+    )
+}
+
+export default Footer;
