148f1bd7fc538effbca7edebd0b696affb67cf64
cryoxe <71180076+cryoxe@users.noreply.github.com>
2024-04-04 20:20:49 +0200

    Removed arrow animation from Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import 'bulma/css/bulma.min.css';
-import '../Arrow.css';
 
 function Home() {
   return (
@@ -13,29 +12,10 @@ function Home() {
 
     <section class="section is-medium">
       <nav class="level">
-
-        <div class="level-left">
-
-          <div class="arrow-sliding-to-right">
-            <div class="arrow-left"></div>
-          </div>
-
-          <div class="arrow-sliding-to-right">
-            <div class="arrow-left"></div>
-          </div>
-
-        </div>
-
         <div class="level-item">
           <button class="button has-background-primary-20 is-gigantic">
             <p>Report now</p>
             </button>
-        </div>
-
-        <div class="level-right">
-          <div class="arrow-sliding-to-left">
-            <div class="arrow-right"></div>
-          </div>
         </div>
       </nav>
     </section>
